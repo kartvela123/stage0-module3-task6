009a4e7354916dba4d08dbff0f71205b08650439
kart <mkartveli9@gmail.com>
2023-02-16 22:21:32 +0000

    3.5

--- src/main/java/lang/print/gaps/task6/PrePostIncrement.java
+++ src/main/java/lang/print/gaps/task6/PrePostIncrement.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task6;
 
 public class PrePostIncrement {
     public static void main(String[] args) {
-        int a = 5;
+        int a = 5 + 1;
         int b = 7;
         int c = 12;
         int d = 15;
